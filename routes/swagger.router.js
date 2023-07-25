2c89b735014ad52ef960df214355d2a231467b52
Prithvi <107529899+earthwebdev@users.noreply.github.com>
2023-07-25 22:17:08 +0545

    Bug: swagger ui custom css url due to include this file as apllication json so it happens

--- routes/swagger.router.js
+++ routes/swagger.router.js
@@ -3,8 +3,12 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger/index.js';
 
 const router = express.Router();
-
+var options = {
+    customCssUrl: [      
+      'https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css'
+    ]
+  };
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
 
 export default router;
